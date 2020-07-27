efe15850026bd6fdf04e8457f78f00af7c78d33a
Jessica Chan <jessica.j.chan@stonybrook.edu>
2020-07-27 14:40:49 -0400

    alert

--- app/static/client.js
+++ app/static/client.js
@@ -31,6 +31,7 @@ function analyze() {
   xhr.onload = function(e) {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
+      alert(response["result"]);
       el("result-label").innerHTML = `Result = ${response["result"]}`;
     }
     el("analyze-button").innerHTML = "Analyze";
